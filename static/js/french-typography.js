a8616b95f25710e1d6a2b0b9dfa1cf8b875184df
Stéphane Lalut <contact@anthropie.fr>
2026-04-08 01:11:11 +0200

    fix: motif desktop visible marges, tableaux responsive mobile, typographie FR insécable

---
+++ static/js/french-typography.js
@@ -0,0 +1,36 @@
+(function() {
+  if (document.documentElement.lang !== 'fr') return;
+
+  var THIN_NBSP = '\u202F';
+  var NBSP = '\u00A0';
+
+  var containers = document.querySelectorAll('main, article, .site-main, .hero');
+
+  function fix(text) {
+    return text
+      .replace(/ ([?!;:])/g, THIN_NBSP + '$1')
+      .replace(/« /g, '\u00AB' + NBSP)
+      .replace(/ »/g, NBSP + '\u00BB')
+      .replace(/ (\u2014|\u2013) /g, NBSP + '$1 ');
+  }
+
+  function walk(node) {
+    if (node.nodeType === 3) {
+      if (node.nodeValue && node.nodeValue.trim().length > 0) {
+        node.nodeValue = fix(node.nodeValue);
+      }
+    } else if (node.nodeType === 1) {
+      var tag = node.tagName.toLowerCase();
+      if (['code', 'pre', 'input', 'textarea', 'script', 'style'].indexOf(tag) !== -1) return;
+      for (var i = 0; i < node.childNodes.length; i++) {
+        walk(node.childNodes[i]);
+      }
+    }
+  }
+
+  if (containers.length === 0) {
+    walk(document.body);
+  } else {
+    containers.forEach(walk);
+  }
+})();
